86602ed30c685426f26228bd1f192859909dd3c5
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-07-31 13:09:47 +0100

    Initial Commit with Starter Code

---
+++ app/src/androidTest/java/com/sriyank/javatokotlindemo/ExampleInstrumentedTest.java
@@ -0,0 +1,28 @@
+package com.sriyank.javatokotlindemo;
+
+import android.content.Context;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+	@Test
+	public void useAppContext() throws Exception {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getTargetContext();
+
+		assertEquals("com.sriyank.javatokotlindemo", appContext.getPackageName());
+	}
+}
